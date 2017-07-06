f22ac0b4ca3306c8a6670871f72b4d179c20d0bd
Szabó Péter <szabopeter@github.com>
2017-07-06 18:35:05 +0200

    Bug repro for not finding all solutions.

--- test_basics.py
+++ test_basics.py
@@ -5,11 +5,20 @@ from solver import Solver
 class BasicsTestCase(unittest.TestCase):
     def test_2x1_trivial(self):
         solver = Solver(""" 2x1:.12
-                            1 | .
-                            -----
-                            . | .""")
+                            1 .
+                            ---
+                            . . """)
         solutions = solver.solve()
         self.assertEqual(1, len(solutions))
+
+    def test_2x1_backtracking(self):
+        solver = Solver(""" 2x1:.12
+                            . .
+                            ---
+                            . . """)
+        solutions = solver.solve()
+        self.assertEqual(2, len(solutions))
+
 
 if __name__ == "__main__":
     unittest.main()
